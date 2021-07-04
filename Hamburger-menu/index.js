dcd39eeaefd2dbcd5ef4df1df2d6cdd815affd67
Glen-stark <76627668+Glen-stark@users.noreply.github.com>
2021-07-04 21:48:34 +0100

    Add files via upload

---
+++ Hamburger-menu/index.js
@@ -0,0 +1,13 @@
+
+let openNav = () => {
+  document.querySelector("#main").style.marginLeft = "250px";
+   document.querySelector("#mySidebar").style.width = "250px";
+    }
+  
+  
+  
+    let closeNav = () => {
+      document.querySelector("#main").style.marginLeft= "0px";
+     document.querySelector("#mySidebar").style.width = "0px";
+      }
+  
