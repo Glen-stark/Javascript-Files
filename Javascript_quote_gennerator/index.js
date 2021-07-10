722169a4b238441768665c4dd0f920702df98348
Glen-stark <76627668+Glen-stark@users.noreply.github.com>
2021-07-10 09:28:28 +0100

    Add files via upload

---
+++ Javascript_quote_gennerator/index.js
@@ -0,0 +1,14 @@
+
+const getQuote = () => {
+    const url =  "https://geek-quote-api.herokuapp.com/v1/quote"
+fetch(url)
+.then(response => response.json())
+.then(data => {document.getElementById("display").innerHTML = data.quote
+});
+
+}
+    
+
+
+
+
